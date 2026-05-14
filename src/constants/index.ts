847cc6816ddc060c60e69476657c6d24d956529b
Sanju333-scar <bheeman1000@gmail.com>
2026-05-15 01:50:29 +0530

    feat: update skills - add C++, Kubernetes, fix social icons

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,45 +6,37 @@ export interface Skill {
 }
 
 export const skills: Skill[] = [
-  { skill_name: 'HTML5', Image: '/html.png', width: 80, height: 80 },
-
-  { skill_name: 'CSS', Image: '/css.png', width: 80, height: 80 },
-
-  { skill_name: 'JavaScript', Image: '/js.png', width: 65, height: 65 },
+  { skill_name: 'HTML', Image: '/htmx.svg', width: 80, height: 80 },
 
   { skill_name: 'React', Image: '/react.png', width: 80, height: 80 },
 
   { skill_name: 'Node.js', Image: '/node-js.png', width: 80, height: 80 },
 
-  { skill_name: 'Python', Image: '/python.png', width: 75, height: 75 },
+  { skill_name: 'Python', Image: '/python.png.svg', width: 75, height: 75 },
 
-  { skill_name: 'FastAPI', Image: '/fastapi.png', width: 75, height: 75 },
+  { skill_name: 'C++', Image: '/cplusplus.svg', width: 75, height: 75 },
 
-  { skill_name: 'Flutter', Image: '/flutter.png', width: 75, height: 75 },
+  { skill_name: 'FastAPI', Image: '/fastapi.svg', width: 75, height: 75 },
 
   { skill_name: 'PostgreSQL', Image: '/postger.png', width: 70, height: 70 },
 
   { skill_name: 'MySQL', Image: '/mysql.png', width: 70, height: 70 },
 
-  { skill_name: 'SQL', Image: '/sql.png', width: 70, height: 70 },
-
   { skill_name: 'Docker', Image: '/docker.webp', width: 70, height: 70 },
+
+  { skill_name: 'Kubernetes', Image: '/kubernetes.png', width: 70, height: 70 },
 
   { skill_name: 'AWS', Image: '/aws.png', width: 70, height: 70 },
 
-  { skill_name: 'Git', Image: '/git.png', width: 70, height: 70 },
+  { skill_name: 'Git', Image: '/git.svg', width: 70, height: 70 },
 
-  { skill_name: 'GitHub Actions', Image: '/githubactions.png', width: 75, height: 75 },
+  { skill_name: 'GitHub Actions', Image: '/githubactions.svg', width: 75, height: 75 },
 
-  { skill_name: 'GitLab', Image: '/gitlab.png', width: 80, height: 80 },
+  { skill_name: 'Linux', Image: '/linux.svg', width: 75, height: 75 },
 
-  { skill_name: 'Linux', Image: '/linux.png', width: 75, height: 75 },
+  { skill_name: 'Azure DevOps', Image: '/azure-devops.png', width: 75, height: 75 },
 
-  { skill_name: 'Azure DevOps', Image: '/azuredevops.png', width: 75, height: 75 },
-
-  { skill_name: 'Bash', Image: '/bash.png', width: 75, height: 75 },
-
-  { skill_name: 'C++', Image: '/cpp.png', width: 70, height: 70 },
+  { skill_name: 'Bash', Image: '/Bash.png', width: 75, height: 75 },
 ]
 
 export const Socials = [
